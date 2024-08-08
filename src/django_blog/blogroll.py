ad8e918ec204726a66a8f523f0ebea2e821f3d89
heyuzhu342 <louiskautiku@gmail.com>
2024-08-08 21:28:02 +0800

    修改链接

--- src/django_blog/blogroll.py
+++ src/django_blog/blogroll.py
@@ -18,4 +18,16 @@ sites = [
     {"url": "http://www.songluyi.com/", "name": "灯塔水母", "desc": "灯塔水母"},
     {"url": "http://blog.topspeedsnail.com/", "name": "斗大的熊猫", "desc": "本博客专注于技术，Linux，编程，Python，C，Ubuntu、开源软件、Github等"},
     {"url": "https://www.urlteam.org/", "name": "URL-team", "desc": "URL-team"},
+    {"url": "https://www.kirinjewelrywholesale.com/", "name": "Kirin Jewelry", "desc": "Kirin Jewelry Co., Ltd is a "
+                                                                                       "high-quality wholesale custom "
+                                                                                       "jewelry manufacturer & "
+                                                                                       "supplier, operating as a "
+                                                                                       "leading jewelry factory since "
+                                                                                       "2006. We specialize in "
+                                                                                       "crafting 925 sterling silver, "
+                                                                                       "gold plated, "
+                                                                                       "and brass jewelry. Welcome to "
+                                                                                       "write to discuss "
+                                                                                       "consultation."},
+
 ]
